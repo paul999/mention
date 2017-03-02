0114f33b80f5be294817742623f24b735d96fbbf
Paul Sohier <paul.sohier@tno.nl>
2017-03-02 13:52:45 +0100

    Lets see if this works

--- styles/all/template/js/mention.js
+++ styles/all/template/js/mention.js
@@ -1,27 +1,25 @@
-(function($) {
-    $(document).ready(function() {
-        console.log("Loading mention settings");
-        $('#message').atwho({
-            at: "@",
-            displayTpl: '@${username}',
-            insertTpl: '[mention]${userid}[/mention]',
-            callbacks: {
-                /*
-                 It function is given, At.js will invoke it if local filter can not find any data
-                 @param query [String] matched query
-                 @param callback [Function] callback to render page.
-                 */
-                remoteFilter: function(query, callback) {
-                    if (query.length < 3) {
-                        callback([]);
-                    }
-                    else {
-                        $.getJSON(U_AJAX_MENTION_URL, {q: query}, function (data) {
-                            callback(data.usernames)
-                        });
-                    }
+$(document).ready(function() {
+    console.log("Loading mention settings");
+    $('#message').atwho({
+        at: "@",
+        displayTpl: '@${username}',
+        insertTpl: '[mention]${userid}[/mention]',
+        callbacks: {
+            /*
+             It function is given, At.js will invoke it if local filter can not find any data
+             @param query [String] matched query
+             @param callback [Function] callback to render page.
+             */
+            remoteFilter: function(query, callback) {
+                if (query.length < 3) {
+                    callback([]);
+                }
+                else {
+                    $.getJSON(U_AJAX_MENTION_URL, {q: query}, function (data) {
+                        callback(data.usernames)
+                    });
                 }
             }
-        });
+        }
     });
 });

9b4be3a63e49a592bdc6e5a5738b58611ce0d8ae
kumencz <kubape12@gmail.com>
2017-04-21 16:42:36 +0200

    Czech translation - non-formal

---
+++ language/cs_nef/common.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * phpBB mentions. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2016, paul999, https://www.phpbbextensions.io
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+/**
+ * As there is no proper way of including this file just when the notification is
+ * loaded we need to include it on all pages. Make sure to only include important
+ * language items (That are directly needed by the notification system) in this file.
+ */
+$lang = array_merge($lang, array(
+	'MENTION_MENTION_NOTIFICATION'	=> 'Byl jsi zmíněn od %s',
+    'NOTIFICATION_TYPE_MENTION'     => 'Někdo mě zmínil'
+));

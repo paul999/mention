c64941997218ec1fd47a72a15a23b4d8cde2e96a
Matt Friedman <maf675@gmail.com>
2017-05-02 00:56:30 -0700

    Change migration dependency

--- migrations/update_notifications.php
+++ migrations/update_notifications.php
@@ -4,7 +4,7 @@
  * phpBB mentions. An extension for the phpBB Forum Software package.
  *
  * @copyright (c) 2016, paul999, https://www.phpbbextensions.io
- * @license       GNU General Public License, version 2 (GPL-2.0)
+ * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
 
@@ -14,7 +14,7 @@ class update_notifications extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
-		return array('\paul999\mention\migrations\update_bbcode');
+		return array('\paul999\mention\migrations\version_100RC2');
 	}
 
 	public function update_data()

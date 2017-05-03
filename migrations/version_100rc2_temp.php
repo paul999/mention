4ba7f64ea25af4e5768a5e5c0073a2479fe87600
Paul Sohier <paul999@phpbb.com>
2017-05-03 15:08:32 +0200

    Rename file

---
+++ migrations/version_100rc2_temp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * phpBB mentions. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2016, paul999, https://www.phpbbextensions.io
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace paul999\mention\migrations;
+
+class version_100Rc2Temp extends \phpbb\db\migration\migration
+{
+
+	static public function depends_on()
+	{
+		return array(
+			'\paul999\mention\migrations\version_001',
+            '\paul999\mention\migrations\update_bbcode',
+		);
+	}
+
+}

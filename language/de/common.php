bc368578f5e5ea298f3504b2c644e7bec7278f7f
Paul Sohier <paul999@phpbb.com>
2017-05-07 10:35:32 +0200

    Update german language with changes to the topic title

--- language/de/common.php
+++ language/de/common.php
@@ -24,6 +24,6 @@ if (empty($lang) || !is_array($lang))
  * language items (That are directly needed by the notification system) in this file.
  */
 $lang = array_merge($lang, array(
-    'MENTION_MENTION_NOTIFICATION'	=> 'Du wurdest von %s erwähnt',
+    'MENTION_MENTION_NOTIFICATION'	=> 'Du wurdest von %1$s erwähnt<br />in “%2$s”',
     'NOTIFICATION_TYPE_MENTION'     => 'Jemand hat dich erwähnt'
 ));

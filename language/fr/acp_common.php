dcd68586131db9cb55811b0127bfbc7c62c44d40
Xavier Olland <xavier.olland@gmail.com>
2019-10-26 18:19:33 +0300

    French translation for ACP

--- language/fr/acp_common.php
+++ language/fr/acp_common.php
@@ -24,10 +24,9 @@ if (empty($lang) || !is_array($lang))
  * language items (That are directly needed by the notification system) in this file.
  */
 $lang = array_merge($lang, array(
-	'MENTION_LENGTH'                => 'Simple mention minimum length',
-	'MENTION_LENGTH_EXPLAIN'        => 'The minimum text length before the simple mention dropdown is shown. 
-	On larger boards you might want to increase this value.',
-	'MENTION_COLOR'                 => 'Simple mention color',
-	'MENTION_COLOR_EXPLAIN'         => 'This color is used within the post to define what user is mentioned. Only hex values can be used.',
-	'MENTION_COLOR_INVALID'         => 'The selected mention color (%s) is invalid. Please select a valid HEX color, without #',
+	'MENTION_LENGTH'                => 'Longueur minimum des mentions',
+	'MENTION_LENGTH_EXPLAIN'        => 'Longueur minimum avant d’afficher les suggestions de mention. Il est conseillé d’augmenter ce nombre sur les forums avec des nombreux utilisateurs.',
+	'MENTION_COLOR'                 => 'Couleur des mentions',
+	'MENTION_COLOR_EXPLAIN'         => 'Cette couleur est utiliser dans le post pour désigner l’utilisateur mentionné. Valeurs hexadécimales uniquement.',
+	'MENTION_COLOR_INVALID'         => 'La couleur choisi (%s) est invalide. Merci de choisir une couleur en valeur HEX, sans le #',
 ));

685b71c3101ac9e8cb64cae6230e607d0e82e012
Mateusz Dutko <mateusz.dutko@gmail.com>
2019-12-09 13:14:55 +0100

    Polish translation

---
+++ language/pl/acp_common.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * phpBB mentions. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2016, paul999, https://www.phpbbextensions.io
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+/**
+ * As there is no proper way of including this file just when the notification is
+ * loaded we need to include it on all pages. Make sure to only include important
+ * language items (That are directly needed by the notification system) in this file.
+ */
+$lang = array_merge($lang, array(
+	'MENTION_LENGTH'                => 'Minimalna długość nazwy użytkownika',
+	'MENTION_LENGTH_EXPLAIN'        => 'Tutaj można ustawić minimalną długość nazwy użytkownika, którą należy wprowadzić, aby wspomnieć użytkownika.
+	Na większych forach możesz zwiększyć tę wartość.',
+	'MENTION_COLOR'                 => 'Kolor wzmianki',
+	'MENTION_COLOR_EXPLAIN'         => 'Ten kolor jest używany w poście, aby określić, który użytkownik jest wspomniany. Mogą być użyte tylko wartości szesnastkowe.',
+	'MENTION_COLOR_INVALID'         => 'Wybrany kolor wzmianki (%s) jest nieprawidłowy. Proszę wybrać prawidłowy kolor HEX bez #',
+));

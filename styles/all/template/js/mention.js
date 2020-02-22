15add531a84687157cfbd35892224c1a69d103ac
paul sohier <paul999@phpbb.com>
2020-02-22 13:40:59 +0100

    Remove console log

--- styles/all/template/js/mention.js
+++ styles/all/template/js/mention.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
         collection: [{
             trigger: '@',
             menuItemTemplate: function (item) {
-                console.log(item);
                 return item.string;
             },
 
